c8b34af8aed91a42713bba216060acd10930e279
lixp <golixp@outlook.com>
2026-03-17 22:49:54 +0800

    chore(pr.sh): update script to fetch upstream and create new branch for golixp-resume-zh-cn version 0.1.1

--- script/pr.sh
+++ script/pr.sh
@@ -9,16 +9,18 @@ git sparse-checkout set packages/preview/golixp-resume-zh-cn
 git remote add upstream git@github.com:typst/packages
 git config remote.upstream.partialclonefilter tree:0
 
+### 更新
+
+# 将当前分支指向上游
+git fetch upstream main --depth=1
+git checkout -b v0.1.1 upstream/main
+
 # 发布文件
 bash ~/project/typst-resume-zh-cn/script/publish.sh ~/project/typst-official-packages-repo/packages/preview/golixp-resume-zh-cn/0.1.1
 
 # 提交
 git add . --ignore-removal
-git commit -m "resume-zh-cn:0.1.1"
-
-# 拉取上游
-git fetch upstream
-git rebase upstream/main
+git commit -m "golixp-resume-zh-cn:0.1.1"
 
 # 推送
-git push origin main --force
+git push origin v0.1.1
